ce9b71475059ce41540d06ee099c565e35be359a
Monica-R <moniroka_339@hotmail.com>
2025-01-27 16:01:35 +0000

    feature: adding elements html of test

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,12 @@
 console.log('Hola mundo!');
+
+let angle = 0; //el ángulo del giro, empieza en 0
+let angleRotateWheel = document.querySelector('.wheel'); 
+
+function rotateWheel() {
+    angle += 5;
+    angleRotateWheel.style.transform = `rotate(${angle}deg)`;
+    requestAnimationFrame(rotateWheel);
+}
+
+//rotateWheel();
