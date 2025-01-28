86818fdbbcfd9c4991fb698dde532817e4af526f
Monica-R <moniroka_339@hotmail.com>
2025-01-28 12:48:27 +0000

    feat: class Target added

--- assets/js/target.js
+++ assets/js/target.js
@@ -1,6 +1,28 @@
 export class Target {
-    constructor (posTargetX, posTargetY) {
+    constructor (posTargetX, posTargetY, angle, targetElement) {
         this.posTargetX = posTargetX;
         this.posTargetY = posTargetY;
+        this.angle = angle; // ángulo de rotación del target - number
+        this.isHit = false; // estado del target - boolean
+        this.targetElement = targetElement;
+    }
+    
+    getTargetCoords() {
+        return {
+            x: this.posTargetX,
+            y: this.posTargetY
+        }
+    }
+    
+    updatePosition(wheelTargetX, wheelTargetY, radius){
+        this.posTargetX = wheelTargetX + radius * Math.cos(this.angle);
+        this.posTargetY = wheelTargetY + radius * Math.sin(this.angle);
+
+        this.targetElement.style.left = `${this.posTargetX}px`;
+        this.targetElement.style.top = `${this.posTargetY}px`;
+    }
+
+    markAsHit() {
+        this.isHit = true;
     }
 }
